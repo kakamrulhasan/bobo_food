66f94b90db98a571dcf84c1aadcddd324d6abad5
Kamrul <kamrul2000@gmail.com>
2026-05-07 19:15:56 +0600

    feat: adding cart . notification . Favorite BottomNav Empty Screens

--- lib/presentation/notification/view/notification_screen.dart
+++ lib/presentation/notification/view/notification_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../core/constansts/color_manager.dart';
+import '../../../core/constansts/icon_manager.dart';
+import '../../../core/constansts/style_manager.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/custom_icon_text_header.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -10,6 +18,42 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.whiteColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // ============ Header Section ============
+              CustomIconTextHeader(icon: IconManager.love, text: 'Favorites'),
+              Spacer(),
+              // ========== Empty Box Section ===========
+              SvgPicture.asset(
+                IconManager.emptyBox,
+                width: 200.w,
+                height: 200.h,
+              ),
+              SizedBox(height: 20.h),
+              // =========== Title & SubTitle ===========
+              Padding(
+                padding: EdgeInsets.all(16.0.w),
+                child: Text(
+                  'Nothing found here!',
+                  textAlign: TextAlign.center,
+                  style: getBoldStyle24(
+                    color: ColorManager.typography500,
+                    fontSize: 32.sp,
+                  ).copyWith(wordSpacing: -2),
+                ),
+              ),
+
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
